8e8849ee670aadc627c7357229166192f1364d08
Rifqi22424 <115063938+Rifqi22424@users.noreply.github.com>
2023-05-05 10:53:13 +0700

    Add files via upload

---
+++ BelajarQueue.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class BelajarQueue {
+    public static void main(String[] args) {
+        Queue<String> todolist = new LinkedList<>();
+
+        todolist.add("Bangun tidur");
+        todolist.add("Sarapan");
+        todolist.add("Mandi");
+        todolist.add("Shalat dhuha");
+        todolist.add("Kuliah");
+        todolist.add("Makan");
+        todolist.add("Istirahat");
+        todolist.add("Olahraga");
+        todolist.add("Main");
+        todolist.add("Tidur");
+
+        System.out.println("To do list kegiatan seahari-hari:");
+        while (!todolist.isEmpty()) {
+            String kegiatan = todolist.remove();
+            System.out.println("- " + kegiatan);
+        }
+
+    }
+}
